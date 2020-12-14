a2f405cffd19f11c492b3776147a1f0e0a947a12
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-12-14 17:37:10 -0500

    tailor edit,show,update,destroy actions added

--- app/controllers/tailors_controller.rb
+++ app/controllers/tailors_controller.rb
@@ -1,4 +1,5 @@
 class TailorsController < ApplicationController
+  before_action :set_tailor, only:[:show, :edit, :update, :destroy]
   def index
     @tailors = Tailor.all
   end
@@ -17,12 +18,31 @@ class TailorsController < ApplicationController
   end
 
   def show
-    @tailor = Tailor.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @tailor.update(tailor_params)
+      redirect_to tailors_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @tailor.destroy
+    redirect_to tailors_path
   end
 
   private
 
   def tailor_params
     params.require(:tailor).permit(:shop_name, :description, :review, :rating, :city)
+  end
+
+  def set_tailor
+    @tailor = Tailor.find(params[:id])
   end
 end
