20bddca4b4bf6bcd8c7f7e887a6a019d5a6f30de
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-12-17 17:42:15 -0500

    Geocoding gem installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_200131) do
+ActiveRecord::Schema.define(version: 2020_12_17_223617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,9 @@ ActiveRecord::Schema.define(version: 2020_12_16_200131) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "city"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
