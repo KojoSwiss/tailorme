9ca0e0e35796ffe7ff77eea74314f5c61dab51b6
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-12-21 12:14:06 -0500

    Destroy comment

--- app/models/tailor.rb
+++ app/models/tailor.rb
@@ -1,5 +1,5 @@
 class Tailor < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
